2843d174d3cca981bbab9c0678a574472f750f4c
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-09-02 17:08:19 +0200

    restaurant details screen

--- redux/cart/cartSlice.ts
+++ redux/cart/cartSlice.ts
@@ -10,7 +10,6 @@ export const cartSlice = createSlice({
     reducers: {
         addToCart: (state, action) => {
             state.items = [...state.items, action.payload]
-
         },
         removeFromCart: (state, action) => {
             const itemIndex = state.items.findIndex((item) => item.id === action.payload.id)
